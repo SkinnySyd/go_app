8ee671c36497bbdff0ea1cc10d41269991798eaa
skinnysyd <rachik1999@gmail.com>
2024-05-02 12:03:59 +0100

    first app upload

---
+++ dockerfile
@@ -0,0 +1,27 @@
+# Build the application
+FROM golang:1.22.2 AS build
+
+WORKDIR /app
+
+COPY go.mod go.sum ./
+RUN go mod download
+
+COPY *.go  ./
+COPY templates /app/templates
+
+RUN CGO_ENABLED=0 GOOS=linux go build -o /docker-hello
+
+# Deploy the application binary into a lean image
+FROM gcr.io/distroless/base-debian11 AS release
+
+WORKDIR /
+
+COPY --from=build /docker-hello /docker-hello
+COPY --from=build /app/templates /templates
+
+
+EXPOSE 8080
+
+USER nonroot:nonroot
+
+ENTRYPOINT ["/docker-hello"]
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+)
+
+func authMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+		isLoggedIn := session.Get("isLoggedIn")
+
+		if isLoggedIn != true {
+			c.Redirect(http.StatusFound, "/login")
+			c.Abort()
+		} else {
+			c.Next()
+		}
+	}
+}
+
+func main() {
+
+	r := gin.Default()
+
+	store := cookie.NewStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
+
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/", authMiddleware(), func(c *gin.Context) {
+		c.String(http.StatusOK, "hello world")
+	})
+
+	r.GET("/login", func(c *gin.Context) {
+		session := sessions.Default(c)
+		isLoggedIn := session.Get("isLoggedIn")
+		if isLoggedIn == true {
+			c.Redirect(http.StatusFound, "/")
+			return
+		}
+		c.HTML(http.StatusOK, "login.tmpl", gin.H{})
+	})
+
+	r.POST("/login", func(c *gin.Context) {
+		email := c.PostForm("email")
+		password := c.PostForm("password")
+
+		// TODO: validate user's credentials
+		if email == "user@mail" && password == "password" {
+			// Successful login
+			session := sessions.Default(c)
+			session.Set("isLoggedIn", true)
+			session.Save()
+			c.Redirect(http.StatusFound, "/")
+		} else {
+			// Invalid credentials
+			c.HTML(http.StatusBadRequest, "login.tmpl", gin.H{
+				"errorMessage": "Invalid email or password",
+			})
+		}
+	})
+
+	r.Run()
+}
